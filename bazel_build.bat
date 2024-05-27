67c7bf88ce47d5291fb33212b8100f63723413da
sjhgithub <jianhongsong@163.com>
2024-05-27 10:02:36 +0800

    https://tensorflow.google.cn/install/source_windows #注意该页面应使用英文,中文的话可能是老版本的步骤
    bazel clean --expunge
    
    可根据.bazelversion下载bazel版本
    
    set http_proxy=http://127.0.0.1:10809
    set https_proxy=http://127.0.0.1:10809
    
    bazel --output_user_root="E:/bazel_root" build --noincompatible_do_not_split_linking_cmdline --config=windows --config=win_clang --verbose_failures --config=opt [--config=dbg] --local_ram_resources=2048
    ■//tensorflow:tensorflow.dll //tensorflow:tensorflow.lib //tensorflow:install_headers
    ■//tensorflow:tensorflow_cc(在版本2.16.1失败,提示链接时找不到导入函数)
    ■//tensorflow/examples/label_image(例子)
    ■//tensorflow/tools/pip_package:wheel --repo_env=WHEEL_NAME=tensorflow_cpu
    
    # 注意: 由于windows系统文件路径长度限制,需要设置output_user_root
    # 注意: 如果在windows系统使用pyenv配置python,编译(bazel build ...)时会提示CreateProcessW(python3...)失败(ERROR: src/main/native/windows/process.cc(202))
    原因是找到的python3可执行脚本是linux的,需要修改"~\_bazel_Administrator\brzvmudz\external\llvm-raw\utils\bazel\configure.bzl"
    ...
    python_bin = repository_ctx.which("python3") 改为 python_bin = repository_ctx.which("python3.bat")
    ...

---
+++ bazel_build.bat
@@ -0,0 +1,28 @@
+@echo off
+@chcp 936
+
+SET CC1=win_clang
+SET CC2=win_mingw
+SET CC3=win_msys
+SET ROT=E:/bazel_root
+SET TGT=//tensorflow:tensorflow.dll //tensorflow:tensorflow.lib //tensorflow:install_headers //tensorflow/tools/pip_package:wheel --repo_env=WHEEL_NAME=tf_nightly_cpu //tensorflow/lite:tensorflowlite //tensorflow/lite/delegates/flex:tensorflowlite_flex
+SET CMD=bazel --output_user_root="%ROT%" build --noincompatible_do_not_split_linking_cmdline --config=windows --config=%CC1% --verbose_failures --config=opt --local_ram_resources=2048
+
+echo --------------------------------------------------------
+echo ----------------------Debug-----------------------------
+echo --------------------------------------------------------
+echo %CMD% --config=dbg %TGT%
+echo --------------------------------------------------------
+echo ---------------------Release----------------------------
+echo --------------------------------------------------------
+echo %CMD% %TGT%
+echo --------------------------------------------------------
+echo ----------------------Debug-----------------------------
+echo --------------------------------------------------------
+%CMD% --config=dbg %TGT%
+echo --------------------------------------------------------
+echo ---------------------Release----------------------------
+echo --------------------------------------------------------
+%CMD% %TGT%
+
+pause
